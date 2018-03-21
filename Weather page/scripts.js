279a5fae4bf3bd6bfcd66104305a3c420da0796d
Irikos <andrei.bardezi@gmail.com>
2018-03-21 17:13:14 +0200

    weather page

--- Weather page/scripts.js
+++ Weather page/scripts.js
@@ -4,8 +4,8 @@ pictures = [
         "image": "./assets/cloudy-weather.jpg"
     },
     {
-        "name": "mountain",
-        "image": "./assets/mountain-weather.jpg"
+        "name": "rainy",
+        "image": "./assets/rainy-weather2.jpg"
     },
     {
         "name": "night",
@@ -13,25 +13,59 @@ pictures = [
     },
     {
         "name": "storm",
-        "image": "./assets/storm-weather.jpg",
+        "image": "./assets/storm-weather2.jpg"
     },
     {
-        "name": "winter",
+        "name": "snow",
         "image": "./assets/winter-weather.jpg"
     },
+    {
+        "name": "fog",
+        "image": "./assets/ceata-weather.jpg"
+    },
+    {
+        "name": "hot",
+        "image": "./assets/hot-weather.jpg" 
+    },
+    {
+        "name": "warm",
+        "image": "./assets/sunny-weather.jpg"
+    },
+    {
+        "name": "cold",
+        "image": "./assets/winter-weather.jpg"
+    },
+    {
+        "name": "Frast",
+        "image":"./assets/winter-weather.jpg"
+    }
 ];
 
 function fahrenheitToCelsius(fahrenheit) {
     var celsius = (5* (fahrenheit - 32)) / 9;
     return celsius;
 };
-
 function getWeather(data) {
     $.getJSON('http://api.openweathermap.org/data/2.5/weather', { q: data.city, appid: "e107aadc09226b858fa632ec65fe4b33" }, showWeather);
 };
+function chooseImage(result) {
+    result.main.temp = fahrenheitToCelsius(result.main.temp);
+    if (result.weather[0].main == "Rain") {
+        $("#background").css("background-image", "url(' ./assets/rainy-weather2.jpg')");  
+    } else if(result.weather[0].main == "Clear") {
+        $("#background").css("background-image", "url(' ./assets/sunny-weather.jpg')");
+    } else if(result.weather[0].main == "Haze") {
+        $("#background").css("background-image", "url(' ./assets/ceata-weather.jpg')");
+    } else if(result.weather[0].main == "Clouds") {
+        $("#background").css("background-image", "url(' ./assets/cloudy-weather.jpg')");
+    } else if(result.weather[0].main == "Snow") {
+        $("#background").css("background-image", "url(' ./assets/winter-weather.jpg')");
+
+}
 
 function showWeather(result) {
     var degrees = fahrenheitToCelsius(result.main.temp);
+    $("#info").text(result.weather[0].description);
     $("#btn").click(function() {
         var type = $("#btn").text();
         console.log(type);
@@ -45,15 +79,8 @@ function showWeather(result) {
         }
         });
     console.log(result);
-    if (result.main.temp > 250) {
-        $("#info").text("Warm");
-    }
-    else {
-        $("#info").text("Cold");
-    }
     $("#background").fadeOut(0, function() {
         var picturesNr = Math.floor(Math.random() * pictures.length);
-        $("#background").css("background-image", "url(" + pictures[picturesNr].image + ")");
         $("#icon").css("weather-icons", "./weather-icons.min.css")
         $("#temperature").text(result.main.temp);
         $("#pressure").text(result.main.pressure);
@@ -65,14 +92,15 @@ function showWeather(result) {
         $("#sunrise").text(result.sys.sunrise);
         $("#sunset").text(result.sys.sunset);
         $("#fahrenheit-celsius").text(result.main.temp);
+        chooseImage(result);
     });
     $("#background").fadeIn(2000);
     var picturesNr = Math.floor(Math.random() * pictures.length);
-    $("#background").css("background-image", "url(" + pictures[picturesNr].image + ")");
-}
+ }
 
 function changeCity() {
-    city = "Bucharest";
+    city = "Kongo";
+    $("#local-location").text(city);
     return city;
 };
 
@@ -83,4 +111,4 @@ $(document).ready(function() {
     getWeather(positionData);   
 
     
-});
+})};
